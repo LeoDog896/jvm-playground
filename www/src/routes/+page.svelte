24a3f5b8063c5ebd78fc84837340f9e37ecd14b7
Tristan F <LeoDog896@hotmail.com>
2023-01-10 20:06:14 -0500

    init

---
+++ www/src/routes/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+    import Monaco from "$lib/Monaco.svelte";
+</script>
+
+<Monaco language={"java"}/>
+
+<style global>
+    :global(body, html) {
+        padding: 0;
+        margin: 0;
+        width: 100%;
+        height: 100%;
+    }
+</style>
